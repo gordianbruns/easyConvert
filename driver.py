6af9c997fecbd7c23feaf319f1639928f9e46f22
Gordian Bruns <gordob1@web.de>
2022-04-04 22:33:15 -0400

    added data

--- driver.py
+++ driver.py
@@ -1,6 +1,179 @@
-from imageProcessing import processImage
+from imageProcessing import *
+from ocrAlgorithms import knn, neural_network_classifier
+from emnist import extract_training_samples, extract_test_samples
+from helpers.common import *
+import os
+from PIL import Image
+import time
+import pytesseract
+
+DEBUG = True
+
+if DEBUG:
+    import numpy as np
+
+
+    def read_image(path):
+        image = Image.open(path).convert('L')
+        image = image.resize((32, 32))
+        image = add_borders(image)
+        return np.asarray(image)
+
+
+    def write_image(image, path):
+        img = Image.fromarray(np.array(image), 'L')
+        img.save(path)
+
+
+ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z']
+
+DATA_DIR = 'data/'
+CHAR_DIR = 'char/'
+TEST_DIR = 'test/'
+TEST_DATA_DIGITS_FILENAME = DATA_DIR + 't10k-images.idx3-ubyte'
+TEST_DATA_DIGITS_LABEL = DATA_DIR + 't10k-labels.idx1-ubyte'
+TRAIN_DATA_DIGITS_FILENAME = DATA_DIR + 'train-images.idx3-ubyte'
+TRAIN_DATA_DIGITS_LABEL = DATA_DIR + 'train-labels.idx1-ubyte'
+TEST_DATA_CHARS_FILENAME = DATA_DIR + CHAR_DIR + 'emnist-digits-test-images-idx3-ubyte'
+TEST_DATA_CHARS_LABEL = DATA_DIR + CHAR_DIR + 'emnist-digits-test-labels-idx1-ubyte'
+TRAIN_DATA_CHARS_FILENAME = DATA_DIR + CHAR_DIR + 'emnist-digits-train-images-idx3-ubyte'
+TRAIN_DATA_CHARS_LABEL = DATA_DIR + CHAR_DIR + 'emnist-digits-train-labels-idx1-ubyte'
 
 
 def run(argv):
-    processImage(argv[0])
+    process_image(argv[0])
 
+    method = 0  # 0 = neural network
+
+    print("Training with EMNIST dataset . . .")
+    x_train, y_train = extract_training_samples('letters')
+    x_train = x_train[:5000] if method == 0 else x_train[:5000].astype(str)
+    y_train = y_train[:5000] if method == 0 else y_train[:5000].astype(str)
+    #y_train = y_train.astype(str)
+    #x_train = []
+    #x_buffer = list(x_buffer[:50000:2])
+    #y_train = list(y_train[:50000:2])
+    x_test, y_test = extract_test_samples('letters')
+    x_test = x_test[:40000:400]
+    y_test = y_test[:40000:400]
+
+    '''for image in x_buffer:
+        new_image = Image.fromarray(image)
+        new_image = add_borders(new_image)
+        new_image = np.asarray(new_image)
+        binarized_image = binarize_image(new_image, 1)
+        inverted_image = invert_image(binarized_image)
+        x_train.append(flatten_list(inverted_image))'''
+
+    print("Finished training with EMNIST dataset . . .")
+
+    '''correct_predictions = 0
+    config = r"--psm 10 --oem 3"
+
+    start = time.time()
+
+    for image_index in range(len(x_test)):
+        new_image = Image.fromarray(x_test[image_index])
+        prediction = pytesseract.image_to_string(new_image, config=config)
+        print(prediction)
+        if prediction == y_test[image_index]:
+            correct_predictions += 1
+
+    end = time.time()
+
+    print(f'Accuracy: {(correct_predictions/len(x_test)) * 100}%')'''
+
+    '''print("Starting training with ETL dataset . . .")
+    for letter in ALPHABET:
+        print(f"Training {letter} . . .")
+        for image_name in os.listdir(f'extract/{letter}'):
+            f = os.path.join(f'extract/{letter}', image_name)
+            if os.path.isfile(f):
+                image = read_image(f'{f}')
+                binarized_image = binarize_image(image, 1)
+                inverted_image = invert_image(binarized_image)
+                x_train.append(flatten_list(inverted_image))
+                y_train.append(letter)
+    print("Finished training with ETL dataset . . .")'''
+
+    #if DEBUG and 0 == 1:
+        # Write some images out just so we can see them visually.
+     #   for idx, test_sample in enumerate(x_test):
+      #      write_image(test_sample, f'{TEST_DIR}{idx}.png')
+        #x_test = [read_image(f'{DATA_DIR}our_test.png')]
+        #y_test = [7]
+
+    '''print("----------------------------------------")
+
+    output = open("output.txt", "a")
+
+    for image_name in os.listdir('images/chars'):
+        f = os.path.join('images/chars', image_name)
+        print(image_name)
+        if os.path.isfile(f):
+            image = read_image(f'{f}')
+            bw_im = binarize_image(image, 1)
+            #for i in range(2):
+             #   bw_im = thin_font(bw_im)
+            inverted_image = invert_image(bw_im)
+            cv2.imwrite("images/binarize_test.jpg", inverted_image)
+            y_prediction = knn(x_train, y_train, [flatten_list(inverted_image)], 3)
+            output.write(y_prediction[0])
+            print(y_prediction[0])'''
+
+    '''x_train = extract_features(x_train)
+    x_test = extract_features(x_test)
+
+    start = time.time()
+    y_prediction = knn(x_train, y_train, x_test, 5)
+    end = time.time()
+    print(f'Prediction: {y_prediction}')
+    print(f'Expected: {y_test}')
+    #print(f'Prediction: {[chr(item + 64) for item in y_prediction]}')
+
+    #print(f'Expected: {[chr(item + 64) for item in y_test]}')
+    accuracy = sum([int(y_prediction_i == y_test_i)
+                    for y_prediction_i, y_test_i in zip(y_prediction, y_test)]) / len(y_test)
+
+    print(f'Accuracy: {accuracy * 100}%')'''
+
+    start = time.time()
+    neural_network_classifier(x_train, y_train, x_test, y_test, 3)
+    end = time.time()
+    #print(f'Elapsed time: {end - start}')
+
+
+def extract_data():
+    import struct
+
+    location = DATA_DIR + "ETL1/ETL1C_04"
+    filename = "ETL1C_04"
+
+    RECORD_SIZE = 2052
+    i = 0
+    print("Reading {}".format(location))
+    with open(location, 'rb') as f:
+        while True:
+            s = f.read(RECORD_SIZE)
+            if s is None or len(s) < RECORD_SIZE:
+                break
+            r = struct.unpack(">H2sHBBBBBBIHHHHBBBBHH2016s4x", s)
+            img = Image.frombytes('F', (64, 63), r[20], 'bit', (4, 0))
+            img = img.convert('L')
+            img = img.point(lambda x: 255 - (x << 4))
+            i = i + 1
+            dirname = r[1].decode('utf-8')
+            dirname = dirname.replace('\0', '')
+            dirname = dirname.replace(' ', '')
+            dirname = dirname.replace('\\', 'YEN')
+            dirname = dirname.replace('+', 'PLUS')
+            dirname = dirname.replace('-', 'MINUS')
+            dirname = dirname.replace('*', 'ASTERISK')
+            try:
+                os.makedirs(f"extract/{dirname}")
+            except:
+                pass
+            imagefile = f"extract1/{dirname}/{filename}_{i:0>6}.png"
+            print(imagefile)
+            img.save(imagefile)
---
+++ ocrAlgorithms.py
@@ -0,0 +1,71 @@
+from helpers.algorithmHelpers import *
+
+
+# k-nearest neighbor algorithm
+def knn(x_train: np.ndarray, y_train: np.ndarray, x_test: list, k: int = 3) -> list:
+    y_prediction = []  # store predictions
+    count = 0  # used to keep track of how many characters have been classified
+
+    # compare every test sample to our training data
+    for idx, test_sample in enumerate(x_test):
+        # compute how different the test_sample is from each other character
+        training_distances = get_training_distances(test_sample, x_train)
+        # sort by distance
+        sorted_distance_indices = [
+            pair[0] for pair in sorted(
+                enumerate(training_distances),
+                key=lambda x: x[1])
+        ]
+
+        # find k nearest neighbors
+        neighbors = [y_train[index] for index in sorted_distance_indices[:k]]
+
+        count += 1
+        print(f'Classified {count}/{len(x_test)}')
+
+        # get most frequent classification in neighbors
+        nearest_neighbor = get_most_frequent(neighbors)
+
+        # make prediction
+        y_prediction.append(nearest_neighbor)
+
+    return y_prediction
+
+
+# algorithm using a neural network
+def neural_network_classifier(x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray, y_test: np.ndarray,
+                              n_epochs: int = 3):
+    n_hidden1 = 512  # 1st hidden layer
+    n_hidden2 = 256  # 2nd hidden layer
+    n_hidden3 = 128  # 3rd hidden layer
+    n_output = 27  # output layer (0-9 digits or 26 letters)
+    # -> letters are classified as 1-27; so we need 27 output nodes
+
+    # flatten data
+    x_train = tf.keras.utils.normalize(x_train, axis=1)
+    x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+    # create neural network
+    model = tf.keras.models.Sequential()
+    # input layer with 28 * 28 nodes; this must match resolution of images
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    # 3 hidden layers
+    model.add(tf.keras.layers.Dense(units=n_hidden1, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(units=n_hidden2, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(units=n_hidden3, activation=tf.nn.relu))
+    # output layer
+    model.add(tf.keras.layers.Dense(units=n_output, activation=tf.nn.softmax))
+
+    # transforms neural network into a series of matrix transformations
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # training the model
+    model.fit(x_train, y_train, epochs=n_epochs)
+
+    # measures performance of model
+    accuracy, loss = model.evaluate(x_test, y_test)
+    print(accuracy)
+    print(loss)
+
+    # save the model
+    model.save('model')
